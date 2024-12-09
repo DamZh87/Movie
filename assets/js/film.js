06facc28de8fa97000cc0df4d9601b1794ff09bb
DamZh87 <xiaomidamir@gmail.com>
2024-12-09 23:13:59 +0500

    Уже лучше

--- assets/js/film.js
+++ assets/js/film.js
@@ -1,21 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-
-  
-
-
-
-
-
-const movie_id = 912649;
-              
-          
-          
-
-
-
-
-
+        
+        
+        const movie_id = JSON.parse(localStorage.getItem("filmId"));
 
         const options = {
           method: 'GET',
@@ -25,52 +11,35 @@ const movie_id = 912649;
           }
         };
         
-        
+
         fetch(`https://api.themoviedb.org/3/movie/${movie_id}`, options)
-          .then(res => res.json())
-           .then(res => {
-            let info = res
-            console.log(info);
-
-
+          .then(filmById => filmById.json())
+          .then(filmById => {
+            let mainFilmInfo = filmById
+            //console.log(mainFilmInfo);
+            let genres = filmById.genres;
+            //console.log(genres);
           
-
             const backdrop_pathEl = document.createElement('img');
-            backdrop_pathEl.setAttribute('src',  `https://image.tmdb.org/t/p/w500/${info.backdrop_path}`);
+            backdrop_pathEl.setAttribute('src',  `https://image.tmdb.org/t/p/w500/${mainFilmInfo.backdrop_path}`);
 
             const poster_pathEl = document.createElement('img');
-            poster_pathEl.setAttribute('src', `https://image.tmdb.org/t/p/w500/${info.poster_path}`);
+            poster_pathEl.setAttribute('src', `https://image.tmdb.org/t/p/w500/${mainFilmInfo.poster_path}`);
 
             const titleEl = document.createElement('p');
-            titleEl.textContent = ('Название: '+ info.title);
+            titleEl.textContent = ('Название: '+ mainFilmInfo.title);
 
             const overviewEl = document.createElement('p');
-            overviewEl.textContent = ('Описание: '+ info.overview);
+            overviewEl.textContent = ('Описание: '+ mainFilmInfo.overview);
 
             const release_dateEl = document.createElement('p');
-            release_dateEl.textContent = ('Дата выхода: '+ info.release_date);
-
+            release_dateEl.textContent = ('Дата выхода: '+ mainFilmInfo.release_date);
 
             const vote_averageEl = document.createElement('p');
-            vote_averageEl.textContent = ('Оценка: '+ info.vote_average);
+            vote_averageEl.textContent = ('Оценка: '+ mainFilmInfo.vote_average);
 
-
-
-           
-           
             const genreEl = document.createElement('p');
-
-          let genre = JSON.stringify(info.genres);
-
-            
-    
-
-
-  
-          console.log(info.genre)
-            
-            genreEl.textContent = (genre);
-            
+            genreEl.textContent = genres;
             
 
             document.body.append(backdrop_pathEl)
@@ -80,14 +49,11 @@ const movie_id = 912649;
             document.body.append(release_dateEl)
             document.body.append(genreEl)
             document.body.append(vote_averageEl)
-
-
         })
-          .catch(err => console.error(err));
-        
-        
+          
+            .catch(err => console.error(err));
 
-          const testEl = document.querySelector('.test');
+          const actorsEl = document.querySelector('.actors');
           function renderActorsCard({
               name = '',
               character ='',
@@ -95,79 +61,60 @@ const movie_id = 912649;
           }) 
               {
               const card = document.createElement('div');
-
-              
-         
+                      
               const profile_pathEl = document.createElement('img');
-              
+              profile_pathEl.setAttribute('src', `https://image.tmdb.org/t/p/w500/${profile_path}`);
+
               const nameEl = document.createElement('h3');
+              nameEl.textContent = name;
+
               const characterEl = document.createElement('h4');
+              characterEl.textContent = character;
+
               card.className = 'user__card'
               nameEl.className = 'user__name'
               profile_pathEl.className = 'profile'
-
               characterEl.className = 'p_small'        
           
-              nameEl.textContent = name;
-        
-              characterEl.textContent = character;
-              profile_pathEl.setAttribute('src', `https://image.tmdb.org/t/p/w500/${profile_path}`);
-
-              
               card.appendChild(nameEl)
-        
               card.appendChild(characterEl)
               card.appendChild(profile_pathEl)
               return card;
-                
-            }
+              }
             
 
           fetch(`https://api.themoviedb.org/3/movie/${movie_id}/credits`, options)
-          .then(res1 => res1.json())
-          .then(res1 => {
-           getActorList(res1.cast)
-         
-           console.log(res1.cast);
-           
-           
+          .then(FilmActors => FilmActors.json())
+          .then(FilmActors => {
+           getActorList(FilmActors.cast)
+          //console.log(FilmActors.cast);
           })
           .catch(err => console.error(err));
        
-                 
-
-
-
-
-            function getActorList(list) {
+          function getActorList(list) {
               let slider = document.querySelector("#myRange");
               let output = document.querySelector("#demo");
               output.innerHTML = slider.value; 
-               
-              
-              let col = 7; 
-            
+                             
+              let actorsQuantity = 1; 
+              actorsEl.innerHTML = ''; 
+              let actorsList = list.slice(0, actorsQuantity);
+              actorsList.forEach(item => {
+              const card = renderActorsCard(item); 
+              actorsEl.appendChild(card);
+            });
+
               slider.oninput = function () {
                   output.innerHTML = this.value;  
-                  col = this.value;  
-              
-              
-              console.log(col); 
-
-
-              testEl.innerHTML = ''; 
-              const tenActors = list.slice(0, col);
-
-
-              tenActors.forEach(item => {
-                const card = renderActorsCard(item); 
-                testEl.appendChild(card);
-        
+                  actorsQuantity = this.value;  
+                  actorsEl.innerHTML = ''; 
+                  let actorsList = list.slice(0, actorsQuantity);
+                  actorsList.forEach(item => {
+                  const card = renderActorsCard(item); 
+                  actorsEl.appendChild(card);
               });
             };
-
-
-            }
+          }
             
 
 });
--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  
-
-
-
   const listEl = document.querySelector('.list');
   function renderMovieCard({
       title = '',
@@ -12,143 +8,125 @@ document.addEventListener('DOMContentLoaded', () => {
       poster_path = '',
       id ='',
       
-      
   }) 
       {
       const card = document.createElement('div');
+
       const titleEl = document.createElement('h3');
+      titleEl.textContent = title;
+
       const overviewEl = document.createElement('p');
+      overviewEl.textContent = overview;
+
       const release_date_nameEl = document.createElement('middle');
+      release_date_nameEl.textContent = release_date;
+
       const poster_pathEl = document.createElement('img');
+      poster_pathEl.setAttribute('src', `https://image.tmdb.org/t/p/w500/${poster_path}`);
+
       const idEl = document.createElement('p');
-    
+      
+      card.setAttribute('data-id', id)
   
-      card.className = 'user__card'
+      
       titleEl.className = 'user__name'
       overviewEl.className ='city_name'
       release_date_nameEl.className ='company_name'
       poster_pathEl.className ='poster'
       idEl.className = 'film_id'
-       
-  
-      titleEl.textContent = title;
-      poster_pathEl.setAttribute('src', `https://image.tmdb.org/t/p/w500/${poster_path}`);
-      overviewEl.textContent = overview;
-      release_date_nameEl.textContent = release_date;
-      card.setAttribute('data-id', id)
-      
-      
-      
-      
+            
+        
       card.appendChild(titleEl)
       card.appendChild(overviewEl)
       card.appendChild(release_date_nameEl)
       card.appendChild(poster_pathEl)
       card.appendChild(idEl)
       return card;
-        
     }
 
- 
-
  let page = 1;
- 
 
-
- main()
+    main()
 
   document.querySelector('.btn').addEventListener('click', () => {
-
     page++
-
-    console.log(page);
+    //console.log(page);
     main()
-  
-
-
  })
-
 
  function main() {
 
-  document.querySelector('.btn').textContent = page;
-  
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZThmMTU3ZTYwMjNmODdlYTdiNWU3MGQ5MjNmOTBmOCIsIm5iZiI6MTczMzQ1NDI3NC40NDYsInN1YiI6IjY3NTI2OWMyZmExMDdkYzRlZDQwNDgxNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FN8b3fNjIHpryFON3ztkvAbJXGz_ag1L79WefRFAXOU'
-    }
-  };
-  
-  
-  fetch(`https://api.themoviedb.org/3/movie/popular?include_adult=false?language=en-US&page=${page}`, options)
-    .then(res => res.json())
-     .then(res => {
-      console.log(res);  
-      getMovieList(res.results);  
-     })
-    .catch(err => console.error(err));
-  
-  
-    function getMovieList(list) {
-      listEl.innerHTML = ''; 
-      list.forEach(item => {
-        const card = renderMovieCard(item); 
-        listEl.appendChild(card);
+          document.querySelector('.btn').textContent = page;
+            const options = {
+            method: 'GET',
+            headers: {
+              accept: 'application/json',
+              Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZThmMTU3ZTYwMjNmODdlYTdiNWU3MGQ5MjNmOTBmOCIsIm5iZiI6MTczMzQ1NDI3NC40NDYsInN1YiI6IjY3NTI2OWMyZmExMDdkYzRlZDQwNDgxNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FN8b3fNjIHpryFON3ztkvAbJXGz_ag1L79WefRFAXOU'
+            }
+          };
+          
+          fetch(`https://api.themoviedb.org/3/movie/popular?include_adult=false?language=en-US&page=${page}`, options)
+            .then(movieList => movieList.json())
+            .then(movieList => {
+              console.log(movieList);  
+              getMovieList(movieList.results);  
+            })
+            .catch(err => console.error(err));
+          
+          
+            function getMovieList(list) {
+              listEl.innerHTML = ''; 
+              list.forEach(item => {
+                const card = renderMovieCard(item); 
+                listEl.appendChild(card);
+                card.addEventListener('click', () => {
+                  const filmId = card.getAttribute('data-id');
+                  location.href='film.html';
+                  localStorage.setItem('filmId', filmId) ;
+                });
+              });
+            }
 
-       
-      
+           
 
-      });
-    }
   }
 
 
-  
-
   const input = document.querySelector("#find");
   input.addEventListener("input", updateValue);
+
+
   function updateValue(e) {
-  let find = e.target.value;
-  console.log(find);
-  
+            let find = e.target.value;
+            console.log(find);
+            
 
-  // document.querySelector('#search').addEventListener('click', () => {
-
-    
-  // let find = document.querySelector('#find').value;
-
-  
-  
-    
-    const options = {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZThmMTU3ZTYwMjNmODdlYTdiNWU3MGQ5MjNmOTBmOCIsIm5iZiI6MTczMzQ1NDI3NC40NDYsInN1YiI6IjY3NTI2OWMyZmExMDdkYzRlZDQwNDgxNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FN8b3fNjIHpryFON3ztkvAbJXGz_ag1L79WefRFAXOU'
-      }
-    };
-    
-    
-    fetch(`https://api.themoviedb.org/3/search/movie?query=${find}`, options)
-      .then(res => res.json())
-       .then(res => {
-        console.log(res);  
-        getMovieList(res.results);  
-       })
-      .catch(err => console.error(err));
-      
-    
-      function getMovieList(list) {
-        listEl.innerHTML = ''; 
-        list.forEach(item => {
-          const card = renderMovieCard(item); 
-          listEl.appendChild(card);
-          
-        });
-      }
-    }
+            // document.querySelector('#search').addEventListener('click', () => {  
+            // let find = document.querySelector('#find').value;
+            const options = {
+                method: 'GET',
+                headers: {
+                  accept: 'application/json',
+                  Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZThmMTU3ZTYwMjNmODdlYTdiNWU3MGQ5MjNmOTBmOCIsIm5iZiI6MTczMzQ1NDI3NC40NDYsInN1YiI6IjY3NTI2OWMyZmExMDdkYzRlZDQwNDgxNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FN8b3fNjIHpryFON3ztkvAbJXGz_ag1L79WefRFAXOU'
+                }
+              };
+              
+                  fetch(`https://api.themoviedb.org/3/search/movie?query=${find}`, options)
+                .then(searchList => searchList.json())
+                .then(searchList => {
+                  console.log(searchList);  
+                  getMovieList(searchList.results);  
+                })
+                .catch(err => console.error(err));
+                    
+                function getMovieList(list) {
+                  listEl.innerHTML = ''; 
+                  list.forEach(item => {
+                    const card = renderMovieCard(item); 
+                    listEl.appendChild(card);
+                  });
+                }
+  }
   
   
   // )
