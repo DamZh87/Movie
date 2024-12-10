4cd083327ad2f82da868f8031c22fe5033865a17
22i01 <22i02samvong@gmail.com>
2024-12-10 22:22:06 +0500

    Add files via upload
    update#1012(ver1.2)

--- project-2/assets/js/main.js
+++ project-2/assets/js/main.js
@@ -1,19 +1,19 @@
-const swiper = new Swiper(".swiper2", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 5500,
-    disableOnInteraction: true,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+// const swiper = new Swiper(".swiper", {
+//   spaceBetween: 30,
+//   centeredSlides: true,
+//   autoplay: {
+//     delay: 5500,
+//     disableOnInteraction: true,
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
 
 
 const swiperMain = document.querySelector("swiper-container");
