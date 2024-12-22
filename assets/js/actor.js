31d1b8e1ee258ed06f85b87cee32e15e6462e0f1
22i01 <22i02samvong@gmail.com>
2024-12-22 22:14:29 +0500

    Add files via upload

---
+++ assets/js/actor.js
@@ -0,0 +1,15 @@
+import { getWeather, getCurrency, footer } from './footer.js';
+import { getActor } from './movieAPI.js';
+
+//ПОЛУЧИТЬ ИНФОРМАЦИЮ ОБ АКТЕРЕ
+getActor()
+
+// ФУТЕР
+footer()
+
+// ПОГОДА ))
+
+getWeather()
+
+//КУРС ВАЛЮТ))
+getCurrency()
