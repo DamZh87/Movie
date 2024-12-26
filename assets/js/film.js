a4dc77a7df225b62a81d8d90df567545ab4d47ed
DamZh87 <xiaomidamir@gmail.com>
2024-12-26 11:42:53 +0500

    Add files via upload

--- assets/js/film.js
+++ assets/js/film.js
@@ -326,6 +326,31 @@ fetch(`https://api.themoviedb.org/3/movie/${movie_id}?language=ru-RU`, options)
 			},
 		});
 	});
+
+
+	const glightbox = GLightbox({
+		touchNavigation: true,
+		loop: true,
+		autoplayVideos: true,
+		openEffect: 'zoom',
+		closeEffect: 'fade',
+		cssEfects: {
+		fade: { in: 'fadeIn', out: 'fadeOut' },
+		zoom: { in: 'zoomIn', out: 'zoomOut' }
+		}
+	});
+
+	const glightbox2 = GLightbox({
+		
+		openEffect: 'zoom',
+		closeEffect: 'fade',
+		cssEfects: {
+		fade: { in: 'fadeIn', out: 'fadeOut' },
+		zoom: { in: 'zoomIn', out: 'zoomOut' }
+		}
+	});
+	
+	
 // footer
 footer()
 // ПОГОДА ))
